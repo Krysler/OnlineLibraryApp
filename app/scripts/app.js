b5510d7e5da96068d93b7c95c3be903a3179b700
krysler <lm.misha.l90@gmail.com>
2015-03-28 17:15:02 +0100

    new file:   .bowerrc 	new file:   .editorconfig 	new file:   .gitattributes 	new file:   .gitignore 	new file:   .jshintrc 	new file:   .travis.yml 	new file:   .yo-rc.json 	new file:   Gruntfile.js 	new file:   README.md 	new file:   app/.buildignore 	new file:   app/.htaccess

---
+++ app/scripts/app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularApp
+ * @description
+ * # angularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ngAria',
+    'ngMessages'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl'
+      })
+      .when('/registration', {
+        templateUrl: 'views/registration.html',
+        controller: 'SignUpCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'login'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
